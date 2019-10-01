6f3788cd2ca6b2f34ef300142d68539f8f06aaba
Michal Wisniewski <mihuwis@gmail.com>
2019-10-01 15:20:17 +0200

    Add Exercise class

--- src/main/java/com/roguethree/gymchampionexerciseap/model/Exercise.java
+++ src/main/java/com/roguethree/gymchampionexerciseap/model/Exercise.java
@@ -2,11 +2,13 @@ package com.roguethree.gymchampionexerciseap.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.roguethree.gymchampionexerciseap.model.enums.BodyPosition;
+import com.roguethree.gymchampionexerciseap.model.enums.Equipment;
+import com.roguethree.gymchampionexerciseap.model.enums.PushPullScheme;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
@@ -17,4 +19,96 @@ public class Exercise {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private Equipment equipment;
+
+    @Enumerated(value = EnumType.STRING)
+    private BodyPosition bodyPosition;
+
+    @Enumerated(value = EnumType.STRING)
+    private PushPullScheme pushPullScheme;
+
+    @ManyToMany
+    @JoinTable(name = "exercise_to_body_part",
+            joinColumns = @JoinColumn(name = "exercise_id"),
+            inverseJoinColumns = @JoinColumn(name = "body_part_id"))
+    private Set<BodyPart> bodyParts = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "exercise_to_muscles",
+            joinColumns = @JoinColumn(name = "exercise_id"),
+            inverseJoinColumns = @JoinColumn(name = "muscle_id"))
+    private Set<Muscle> muscles = new HashSet<>();
+
+    public Exercise(String name, Equipment equipment,
+                    BodyPosition bodyPosition, PushPullScheme pushPullScheme,
+                    Set<BodyPart> bodyParts, Set<Muscle> muscles) {
+        this.name = name;
+        this.equipment = equipment;
+        this.bodyPosition = bodyPosition;
+        this.pushPullScheme = pushPullScheme;
+        this.bodyParts = bodyParts;
+        this.muscles = muscles;
+    }
+
+    public Exercise() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Equipment getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(Equipment equipment) {
+        this.equipment = equipment;
+    }
+
+    public BodyPosition getBodyPosition() {
+        return bodyPosition;
+    }
+
+    public void setBodyPosition(BodyPosition bodyPosition) {
+        this.bodyPosition = bodyPosition;
+    }
+
+    public PushPullScheme getPushPullScheme() {
+        return pushPullScheme;
+    }
+
+    public void setPushPullScheme(PushPullScheme pushPullScheme) {
+        this.pushPullScheme = pushPullScheme;
+    }
+
+    public Set<BodyPart> getBodyParts() {
+        return bodyParts;
+    }
+
+    public void setBodyParts(Set<BodyPart> bodyParts) {
+        this.bodyParts = bodyParts;
+    }
+
+    public Set<Muscle> getMuscles() {
+        return muscles;
+    }
+
+    public void setMuscles(Set<Muscle> muscles) {
+        this.muscles = muscles;
+    }
 }
