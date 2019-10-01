889988ba096680d92a1249792726c8b1f3866483
Michal Wisniewski <mihuwis@gmail.com>
2019-10-01 15:43:03 +0200

    Add exercise service implementation. Under construction

---
+++ src/main/java/com/roguethree/gymchampionexerciseap/services/jpaServices/JpaExerciseService.java
@@ -0,0 +1,30 @@
+package com.roguethree.gymchampionexerciseap.services.jpaServices;
+
+import com.roguethree.gymchampionexerciseap.model.Exercise;
+import com.roguethree.gymchampionexerciseap.services.ExerciseService;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class JpaExerciseService implements ExerciseService {
+    
+    @Override
+    public Optional<Exercise> findByBodyPartName(String bodyPartName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Stream<Exercise> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Exercise> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Exercise> findByName(String name) {
+        return Optional.empty();
+    }
+}
