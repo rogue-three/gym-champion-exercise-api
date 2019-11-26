560e77ddcd4511066932045e9222556486afd076
Michal Wisniewski <mihuwis@gmail.com>
2019-11-26 11:57:04 +0100

    Add adnotations to pushpull controller

--- src/main/java/com/roguethree/gymchampionexerciseap/restcontrollers/PushPullSchemeController.java
+++ src/main/java/com/roguethree/gymchampionexerciseap/restcontrollers/PushPullSchemeController.java
@@ -3,10 +3,15 @@ package com.roguethree.gymchampionexerciseap.restcontrollers;
 import com.roguethree.gymchampionexerciseap.model.PushPullScheme;
 import com.roguethree.gymchampionexerciseap.services.PushPullSchemeService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping(value = "api/v1/pushpullscheme")
 public class PushPullSchemeController implements CrudRestController<PushPullScheme, Long> {
 
     private final PushPullSchemeService ppService;
@@ -16,12 +21,14 @@ public class PushPullSchemeController implements CrudRestController<PushPullSche
     }
 
     @Override
+    @GetMapping
     public ResponseEntity<Set<PushPullScheme>> getAll() {
         Set<PushPullScheme> pushPullSchemes = ppService.findAll().collect(Collectors.toSet());
         return ResponseEntity.ok(pushPullSchemes);
     }
 
     @Override
+    @GetMapping(value = "/id/{id}")
     public ResponseEntity<PushPullScheme> getById(Long id) {
         return ppService.findById(id)
                 .map(ResponseEntity::ok)
@@ -29,6 +36,7 @@ public class PushPullSchemeController implements CrudRestController<PushPullSche
     }
 
     @Override
+    @GetMapping(value = "/name/{name}")
     public ResponseEntity<PushPullScheme> getByName(String name) {
         return ppService.findByName(name)
                 .map(ResponseEntity::ok)
